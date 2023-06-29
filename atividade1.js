b4a9b00fd24a72f46b07d2402cdaf00016f2ec75
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-06-28 22:06:43 -0300

    Atividades do dia 28/06/2023 - Tquinta

---
+++ atividade1.js
@@ -0,0 +1,17 @@
+// 1º)
+var resposta = 23;
+if (resposta >= 18) {
+  console.log("Que legal! Você já pode ter a carteira de habilitação!");
+} else {
+  console.log(
+    "Poxa, que pena! Você ainda não pode ter a carteira de habilitação."
+  );
+}
+
+// 2º)
+var resposta = true;
+if (resposta) {
+  console.log("Poxa, que legal!");
+} else {
+  console.log("Falta pouco! Logo você termina.");
+}
---
+++ atividade3.js
@@ -0,0 +1,22 @@
+// 1º)
+var x = 5;
+var y = 7;
+if (x < y) {
+  console.log("Olá mundo!");
+} else if (x < y) {
+  console.log("Mundo, olá!");
+} else if (x == y) {
+  console.log("Adeus!");
+}
+
+// 2º)
+var fruta = "laranja";
+if (fruta == "banana") {
+  console.log("O filme é Os minions");
+} else if (fruta == "laranja") {
+  console.log("O filme é Laranja Mecânica");
+} else if (fruta == "maçã") {
+  console.log("O filme é Branca de neve");
+} else {
+  console.log("O cinema tá fechado");
+}
